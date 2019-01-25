54965c2f7b779457f32cd2135954ddde406f35ff
RickButer <buter.rick@gmail.com>
2019-01-25 16:38:13 +0100

    friday evening push

--- php/header.php
+++ php/header.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="../css/header.css">
     <title>Welcome to daruma shop</title>
 </head>
-
 <body>
-    <div class="header_container">
-            <img src="images/png/daruma_logo.png" alt="daruma"/>
+    <div class="header_container debug">
+        <div class="logo_container">
+            <img src="../images/png/daruma_logo.png" alt="daruma"/>
+            <div class="navbar_container">
                 <a href="#">test</a>
                 <a href="#">test</a>
                 <a href="#">test</a>
                 <a href="#">test</a>
                 <a href="#">test</a>
+            </div>
         </div>
+    </div>
 </body>
-
 </html>
